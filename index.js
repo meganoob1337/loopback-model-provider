396fa26235772f962dd4bb3b1ead74ea0d215799
Andrei Wilkens <wilkens@silpion.de>
2019-02-14 13:52:08 +0100

    added plugin

---
+++ index.js
@@ -0,0 +1,21 @@
+'use strict';
+const fs = require('fs');
+module.exports = function(loopbackApplication, options) {
+  const excludes = options.excludes || [];
+  const modelPath = __dirname + '/../../common/models/';
+  loopbackApplication.use(options.path, function(req, res, next) {
+    let files = fs.readdirSync(modelPath);
+    files = files.filter((file) => {
+      return file.includes('.json') && !excludes.includes(file.replace('.json', ''));
+    });
+    files = files.reduce((acc, file) => {
+      const model = JSON.parse(fs.readFileSync(modelPath + file));
+      acc[model.name] = {
+        ...model,
+        plural: model.plural || model.name + 's'
+      }
+      return acc;
+    }, {});
+    res.send(JSON.stringify(files));
+  });
+};
